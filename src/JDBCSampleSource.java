2c2f2e7f8f2ba5dd178922031e721d7fad691b80
Alex <43049608+ganbaKotori@users.noreply.github.com>
2019-03-12 13:18:44 -0700

    Changed for Book Database

--- src/JDBCSampleSource.java
+++ src/JDBCSampleSource.java
@@ -36,13 +36,71 @@ public class JDBCSampleSource {
     }
     public static void displayOptions(){
         System.out.println("What would you like to do?");
-        System.out.println("1 : List all video games");
-        System.out.println("2 : List all developers");
+        System.out.println("1 : List all writing groups");
+        System.out.println("2 : List data for a specific group");
         System.out.println("3 : List all publishers");
-        System.out.println("4 : Add a video game");
-        System.out.println("5 : Add a developer");
-        System.out.println("6 : Add a publisher");
+        System.out.println("4 : List data for a specific publisher");
+        System.out.println("5 : List all book titles");
+        System.out.println("6 : List data for a particular book");
+        System.out.println("7 : Insert a new book");
+        System.out.println("8 : Remove a book");
     }
+    
+    public static void listAllBooks(){
+        //STEP 4: Execute a query
+        try{
+            System.out.println("Listing books...");
+            stmt = conn.createStatement();
+            conn = DriverManager.getConnection(DB_URL); //REQUIRED FOR EXECUTION OF STATEMENTS
+            String sql;
+            sql = "SELECT pub_id, pub_name, year_est, city, state, country FROM publishers";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            //STEP 5: Extract data from result set
+            System.out.printf(displayFormat, "Pub ID", "Pub Name", "Year", "City","State","Country");
+            while (rs.next()) {
+                //Retrieve by column name
+                String id = rs.getString("pub_id");
+                String name = rs.getString("pub_name");
+                String year = rs.getString("year_est");
+                String city = rs.getString("city");
+                String state = rs.getString("state");
+                String country = rs.getString("country");
+
+                //Display values
+                System.out.printf(displayFormat+"%n",
+                        id, name, year, city,state,
+                        country); // Doesn't display all columns for some reason
+       
+            }
+            //STEP 6: Clean-up environment
+            rs.close();
+            stmt.close();
+        }catch (SQLException se) {
+            //Handle errors for JDBC
+            se.printStackTrace();
+        } catch (Exception e) {
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        } finally {
+            //finally block used to close resources
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                    return;
+                }
+            } catch (SQLException se2) {
+            }// nothing we can do
+            try {
+                if (conn != null) {
+                    conn.close();
+                    return;
+                }
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }//end finally try
+        }//end try
+    }//END LISTPUBLISHERS
     
     public static void listPublishers(){
         //STEP 4: Execute a query
@@ -98,34 +156,27 @@ public class JDBCSampleSource {
                 se.printStackTrace();
             }//end finally try
         }//end try
-    }
+    }//END LISTPUBLISHERS
     
      public static void insertPublisher(){
         try{
             Scanner in = new Scanner(System.in);
             conn = DriverManager.getConnection(DB_URL); //REQUIRED FOR EXECUTION OF STATEMENTS
-            //PreparedStatement pstmt = null;
-            
-            System.out.println("Enter publisher id");
-            String id = in.nextLine();
+
             System.out.println("Enter publisher name");
             String name = in.nextLine();
-            System.out.println("Enter year publisher was established");
-            String year = in.nextLine();
-            System.out.println("Enter city where the publisher is");
-            String city = in.nextLine();
-            System.out.println("Enter state where the publisher is");
-            String state = in.nextLine();
-            System.out.println("Enter country where the publisher is");
-            String country = in.nextLine();
+            System.out.println("Enter publisher address");
+            String address = in.nextLine();
+            System.out.println("Enter publisher phone number");
+            String phone = in.nextLine();
+            System.out.println("Enter publisher email");
+            String email = in.nextLine();
             
-            PreparedStatement stmt=conn.prepareStatement("insert into publishers values(?,?,?,?,?,?)");  
-            stmt.setString(1,id);//1 specifies the first parameter in the query  
-            stmt.setString(2,name);
-            stmt.setString(3,year);
-            stmt.setString(4,city);
-            stmt.setString(5,state);
-            stmt.setString(6,country);
+            PreparedStatement stmt=conn.prepareStatement("insert into publishers values(?,?,?,?)");  
+            stmt.setString(1,name);//1 specifies the first parameter in the query  
+            stmt.setString(2,address);
+            stmt.setString(3,phone);
+            stmt.setString(4,email);
             
             System.out.println("inserting publisher...");
             
@@ -158,7 +209,7 @@ public class JDBCSampleSource {
                 se.printStackTrace();
             }//end finally try
         }//end try
-    }
+    } //END INSERT PUBLISHER
 
     public static void main(String[] args) {
         //Prompt the user for the database name, and the credentials.
